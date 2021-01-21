feb776b35f64c62b4c4e0b48a45cef60c579d200
Guenther Wannenmacher <guenther.wannenmacher@sap.com>
2021-01-21 17:42:02 +0100

    pipelines

---
+++ .ci/build_and_push_helm.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+import artifactory
+import helm
+import secret_server
+import shutil
+import tempfile
+
+import os
+import subprocess
+import utils
+
+from google.cloud import storage
+from util import ctx
+
+def uploadFolder(packagedChartsDir, bucketname):
+    print("==== Upload directory", packagedChartsDir, "to GCS ====")
+    files = [f for f in os.listdir(packagedChartsDir) if os.path.isfile(os.path.join(packagedChartsDir, f))]
+
+    client = storage.Client()
+    for f in files:
+        print(f"Uploading file {f}")
+        bucket = client.get_bucket(bucketname)
+        testfile = bucket.blob(f)
+        testfile.upload_from_filename(os.path.join(packagedChartsDir, f))
+
+def runHelmIndex(packagedChartsDir, bucketname):
+    """ Build index.yaml for the directory *packagedChartsDir* and write the index file into this directory. """
+    print("==== Building index.yaml for directory", packagedChartsDir, "====")
+    indexFile = packagedChartsDir + os.sep + "index.yaml"
+
+    client = storage.Client()
+    bucket = client.get_bucket(bucketname)
+    blob = bucket.blob("index.yaml")
+
+    if blob.exists():
+        blob.download_to_filename(indexFile)
+        command = ["helm", "repo", "index", "--merge", indexFile, packagedChartsDir]
+    else:
+        command = ["helm", "repo", "index", packagedChartsDir]
+
+    result = subprocess.run(command, capture_output=True)
+    if result.returncode != 0:
+        raise Exception("Cannot build index.yaml: " + str(result))
+
+def prepare_gcs_credentials(gcs_credentials_cfg_name, credentials_file):
+    print("==== Reading GCS credentials from cc-config ====")
+    cfg_factory = ctx().cfg_factory()
+    cfg = cfg_factory._cfg_element(cfg_type_name="gcs", cfg_name=gcs_credentials_cfg_name)
+    gcs_credentials = cfg.raw["password"]
+    credentials_file.write(gcs_credentials)
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.abspath(credentials_file.name)
+
+def packageChartRepo(chart_dir, bucketname):
+    print("== Packaging Repo Directory", chart_dir, "==")
+    runHelmIndex(chart_dir, bucketname)
+    uploadFolder(chart_dir, bucketname)
+
+
+if __name__ == "__main__":
+    print("\n ===== Starting Helm Packaging - Python Powered =====")
+
+    bucketname = "potter_controller_charts"
+    credentials_file_name = "gcs_credentials.json.tmp"
+
+    source_path = os.environ['SOURCE_PATH']
+    version_path = os.environ['VERSION_PATH']
+
+    secret_server_client = secret_server.SecretServer('hub')
+    helm_client = helm.HelmClient()
+
+    chart_version = utils.get_chart_version(source_path, version_path)
+
+    chart_name = "k8s-potter-controller"
+    chart_path = os.path.join(source_path, 'chart', 'controller')
+
+    helm_client.repo_add("bitnami", "https://charts.bitnami.com/bitnami")
+
+    # Now override the place holders in helm chart with concrete values
+    # To not override our source dir copy chart directory to temp dir
+
+    # Note use this for debugging if you want to preserve the temp
+    # directory: temp_out = mkdtemp(prefix="helm_chart_")
+    with tempfile.TemporaryDirectory(prefix="helm_chart_") as temp_out:
+        chart_path_out = os.path.join(temp_out, 'controllerchart')
+        print(f"Rendering helm chart in {chart_path_out}")
+        shutil.copytree(chart_path, chart_path_out)
+
+        utils.replace_chart_placeholder(
+            chart_path=chart_path_out,
+            version_path=version_path,
+            chart_version=chart_version,
+            chart_name=chart_name
+        )
+
+        # helm_client.dependency_update(chart_path=chart_path_out)
+        print("package chart")
+        helm_client.package_chart(chart_path=chart_path_out, out_path=temp_out)
+        tgz_name = chart_name + "-" + chart_version + ".tgz"
+        chart_tgz_path = os.path.join(temp_out, tgz_name)
+
+        with utils.TempFileAuto(prefix="gcs_credentials.json_") as cred_file:
+            prepare_gcs_credentials("hub-chart-pipeline-stable", cred_file)
+            cred_file.switch()
+
+            packageChartRepo(temp_out, "potter-controller-charts")
+
+    print("\n ===== Finished Helm Packaging - Python Over =====")
---
+++ .ci/dev_integration_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+
+root_path = os.getcwd()
+hub_controller_path = os.environ['SOURCE_PATH']
+
+os.environ['HUB_CONTROLLER_PATH'] = os.environ['SOURCE_PATH']
+os.environ['ROOT_PATH'] = root_path
+os.environ['LANDSCAPE'] = "dev"
+os.environ['NAMESPACE'] = "release-test"
+
+
+# hub_kubeconfig = os.path.join(
+#     root_path, hub_controller_path,
+#     ".ci",
+#     "integration_test.py"
+# )
+
+# command = [hub_kubeconfig, "--namespace", "release-test"]
+print("TODO: enenble it as currently DISABLED!")
+
+result = subprocess.run(command)
+result.check_returncode()
