e1ec05ea2f4e1cd6b0a059c61cd407772cd82bc6
Guenther Wannenmacher <guenther.wannenmacher@sap.com>
2021-01-28 13:00:40 +0100

    integration tests

--- .ci/dev_integration_test.py
+++ .ci/dev_integration_test.py
@@ -2,25 +2,40 @@
 
 import os
 import subprocess
+import sys
 
+sys_args = sys.argv
 root_path = os.getcwd()
 hub_controller_path = os.environ['SOURCE_PATH']
+
+# parse sys var(will be passed to subprocess calls)
+for i in range(len(sys_args)):
+    if '--garden-namespace ' in sys_args[i]:
+        os.environ['GARDEN_NAMESPACE'] = sys_args[i].split(' ', 2)[1]
+    if "--target-clustername" in sys_args[i]:
+        os.environ['TARGET_CLUSTER'] = sys_args[i].split(' ', 2)[1]
+    if "--target-cluster-ns1 " in sys_args[i]:
+        os.environ['TARGET_CLUSTER_NS1'] = sys_args[i].split(' ', 2)[1]
+    if "--target-cluster-ns2 " in sys_args[i]:
+        os.environ['TARGET_CLUSTER_NS2'] = sys_args[i].split(' ', 2)[1]
+    if "--test-prefix " in sys_args[i]:
+        os.environ['TEST_PREFIX'] = sys_args[i].split(' ', 2)[1]
+    if "--test-type " in sys_args[i]:
+        os.environ['TEST_TYPE'] = sys_args[i].split(' ', 2)[1]
 
 os.environ['HUB_CONTROLLER_PATH'] = os.environ['SOURCE_PATH']
 os.environ['ROOT_PATH'] = root_path
 os.environ['LANDSCAPE'] = "dev"
-os.environ['NAMESPACE'] = "release-test"
+os.environ['NAMESPACE'] = "controller-release-test"
 
 
-# hub_kubeconfig = os.path.join(
-#     root_path, hub_controller_path,
-#     ".ci",
-#     "integration_test.py"
-# )
+hub_kubeconfig = os.path.join(
+    root_path, hub_controller_path,
+    ".ci",
+    "integration_test.py"
+)
 
-# command = [hub_kubeconfig, "--namespace", "release-test"]
+command = [hub_kubeconfig, "--namespace", "release-test"]
 
-# result = subprocess.run(command)
-# result.check_returncode()
-
-print("TODO: enenble it as currently DISABLED!")
+result = subprocess.run(command)
+result.check_returncode()
---
+++ .ci/integration_test.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+import os
+from subprocess import Popen, PIPE, STDOUT, run
+import sys
+import shutil
+import utils
+import yaml
+
+import secret_server
+from util import ctx
+
+print("Starting integration test")
+controller_path = os.environ['HUB_CONTROLLER_PATH']
+root_path = os.environ['ROOT_PATH']
+landscape = os.environ['LANDSCAPE']
+garden_namespace = os.environ['GARDEN_NAMESPACE']
+target_cluster = os.environ['TARGET_CLUSTER']
+target_cluster_ns1 = os.environ['TARGET_CLUSTER_NS1']
+target_cluster_ns2 = os.environ['TARGET_CLUSTER_NS2']
+test_prefix = os.environ['TEST_PREFIX']
+test_type = os.environ.get('TEST_TYPE', None) # prevent key error if not present
+
+try:
+    # env var is implicitly set by the output dir in case of a release job
+    integration_test_path = os.environ["INTEGRATION_TEST_PATH"]
+except KeyError:
+    print("Output dir env var not set. " +
+          "The output of the integration test won't be saved in a file.")
+
+factory = ctx().cfg_factory()
+landscape_kubeconfig = factory.kubernetes("hub-" + landscape)
+landscape_test_kubeconfig = factory.kubernetes("hub-" + landscape + "-test")
+
+landscape_kubeconfig_name = "landscape_kubeconfig"
+landscape_kubeconfig_path = os.path.join(root_path, hub_path,
+                                         "integration-test",
+                                         landscape_kubeconfig_name)
+
+landscape_test_kubeconfig_name = "landscape_test_kubeconfig"
+landscape_test_kubeconfig_path = os.path.join(root_path, hub_path,
+                                              "integration-test",
+                                              landscape_test_kubeconfig_name)
+
+utils.write_data(landscape_kubeconfig_path, yaml.dump(
+                landscape_kubeconfig.kubeconfig()))
+utils.write_data(landscape_test_kubeconfig_path, yaml.dump(
+                landscape_test_kubeconfig.kubeconfig()))
+
+landscape_config = utils.get_landscape_config("hub-" + landscape)
+int_test_config = landscape_config.raw["int-test"]["config"]
+token = int_test_config["auth"]["token"]
+
+golang_found = shutil.which("go")
+if golang_found:
+    print(f"Found go compiler in {golang_found}")
+else:
+    print("No Go compiler found, installing Go")
+    command = ['apk', 'add', 'go', '--no-progress']
+    result = run(command)
+    result.check_returncode()
+
+
+os.chdir(os.path.join(root_path, controller_path, "integration-test"))
+
+# command = ["go", "run", "main.go",
+#            "--kubeconfig", landscape_kubeconfig_name,
+#            "--target-kubeconfig", landscape_test_kubeconfig_name,
+#            "--token", token, "--namespace", namespace]
+command = ['go', 'run', 'main.go',
+           '-garden-kubeconfig', landscape_test_kubeconfig_path,
+           '-garden-namespace', garden_namespace,
+           '-target-clustername', target_cluster,
+           "-target-cluster-namespace1", target_cluster_ns1,
+           "-target-cluster-namespace2", target_cluster_ns2,
+           "-test-prefix", test_prefix,
+           "-test-type", test_type]
+
+try:
+    # check if path var is set
+    integration_test_path
+except NameError:
+    run = run(command)
+else:
+    output_path = os.path.join(root_path, integration_test_path, "out")
+
+    with Popen(command, stdout=PIPE, stderr=STDOUT, bufsize=1, universal_newlines=True) as run, open(output_path, 'w') as file:
+        for line in run.stdout:
+            sys.stdout.write(line)
+            file.write(line)
+
+if run.returncode != 0:
+    raise EnvironmentError("Integration test exited with errors")
