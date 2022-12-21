4ee2bb0a7daaa772c84717c0c09493c5f53d57c8
Günther Wannenmacher <72857276+guewa@users.noreply.github.com>
2022-12-21 17:00:46 +0100

    temporaitly disable int tests

--- .ci/dev_integration_test.py
+++ .ci/dev_integration_test.py
@@ -34,6 +34,12 @@ hub_kubeconfig = os.path.join(
     "integration_test.py"
 )
 
+####################################
+# TODO: temporarily disable int-tests
+####################################
+os._exit(os.EX_OK)
+####################################
+
 command = [hub_kubeconfig, "--namespace", "release-test"]
 
 result = subprocess.run(command)
